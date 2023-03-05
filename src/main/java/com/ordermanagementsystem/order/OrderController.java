102fd19622893c03d3582d34417ff52a355400b0
ConstMitro <Constantine9@hotmail.co.uk>
2023-03-05 13:22:13 +0100

    Added: -Set up the backend to be able to connect to the DB - Created endpoint to create and update orders in the DB -Added getOrder in OrderService -Added createOrder in OrderService -Added OrderRepository

--- src/main/java/com/ordermanagementsystem/order/OrderController.java
+++ src/main/java/com/ordermanagementsystem/order/OrderController.java
@@ -1,13 +1,33 @@
 package com.ordermanagementsystem.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+    @Autowired
+    private final OrderService service;
+
+    public OrderController(OrderService service) {
+        this.service = service;
+    }
+
+
     @PostMapping("/order/create")
-    public OrderDto CreateOrder(@RequestBody OrderDto orderDto) {
-        return orderDto;
+    public ResponseEntity<String> CreateOrder(@RequestBody OrderDto orderDto) {
+
+        if (service.getOrder(orderDto.getId()).isPresent()){
+            service.createOrder(orderDto);
+            return ResponseEntity.ok(String.format("Your order %s was updated successfully",orderDto.getId()));
+        }
+        else {
+            service.createOrder(orderDto);
+            return ResponseEntity.ok(String.format("Your order %s was created successfully",orderDto.getId()));
+        }
+
     }
 }
+
